5b61dc2a089db27f26f20b0f4b8291482d466ddf
Jorge S. <jsravelo3@gmail.com>
2024-01-13 21:22:17 +0100

    create function that plays the game and tracks score

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // Declare and assign all variables
 
 const choice = ["rock", "paper", "scissors"];
-
-let computerSelection = getComputerChoice();
-
-let playerSelection = "rock"
-
-
+let computerScore = 0;
+let playerScore = 0;
 
 
 // Create function that takes a random choice
@@ -15,6 +11,8 @@ function getComputerChoice() {
     const option = choice[Math.floor(Math.random() * choice.length)];
     return option;
 }
+
+// Create function that checks for winner
 
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -32,6 +30,7 @@ function checkWinner(playerSelection, computerSelection) {
     }
 }
 
+// Create function that plays a round
 
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection);
@@ -46,4 +45,28 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection))
+// Create a function that plays the game a tracks the score
+
+function game() {
+    for (let i=0; i<5; i++) {
+        const playerSelection = prompt();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+
+        if (checkWinner(playerSelection, computerSelection) == "Win") {
+            playerScore++;
+        }
+        else if (checkWinner(playerSelection, computerSelection) == "Lose") {
+            computerScore++;
+        }
+        else {
+            computerScore = computerScore;
+            playerScore = playerScore;
+        }
+    }
+
+}
+
+game()
+console.log(playerScore)
+console.log(computerScore)
