6e08dd337611e42fa1d258aa8cfd4cf7d936239c
Jorge S. <jsravelo3@gmail.com>
2024-01-13 20:15:33 +0100

    create function that checks for winner

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // Declare and assign all variables
 
-const choice = ["rock", "paper", "scissors"]
+const choice = ["rock", "paper", "scissors"];
+
+let computerSelection = getComputerChoice;
+
+// Create function that takes a random choice
 
 function getComputerChoice() {
     const option = choice[Math.floor(Math.random() * choice.length)];
-    console.log(option)
+    return option;
 }
 
-getComputerChoice()
+function checkWinner() {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+        return "Win"
+    }
+    else {
+        return "Lose"
+    }
+}
+
+
+
+funcion playRound (playerSelection, computerSelection) {
+
+}
